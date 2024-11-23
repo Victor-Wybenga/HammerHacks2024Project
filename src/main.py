ad59dca465a85ffab056611ab0a6ea81c343b077
VictorWybenga <wybengavi055@hwcdsb.ca>
2024-11-23 11:02:28 -0500

    Add initial project structure with .gitignore, Makefile, requirements, and main application

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import customtkinter as ctk
+import tkinter.ttk as ttk
+import typing
+import random
+
+def entry(main: typing.Callable[..., None]) -> None:
+    if __name__ == '__main__':
+        main()
+
+class App(ctk.CTk):
+    def __init__(self, title: str, size: tuple[int, int]):
+        super().__init__()
+        self.title(title)
+        self.geometry(f"{size[0]}x{size[1]}")
+        
+        self.button = ctk.CTkButton(
+            self,
+            text="Click Me!",
+            command=self.randomize_button_pos
+        )
+        self.button.place(relx=0.5,rely=0.5)
+    
+    def randomize_button_pos(self):
+        self.button.place(relx=random.random(),rely=random.random())
+        
+
+@entry
+def main() -> None: 
+    app = App("Calculator", (240, 320))
+    app.mainloop()
